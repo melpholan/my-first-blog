a70fda819813a41b17961e7e5dfc1cefa845274f
melpholan <melpholan@yandex.ru>
2016-11-19 17:35:48 +0300

    "Повторно article/templates/article/post_list.html"

--- article/views.py
+++ article/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
+
+def post_list(request):
+    return render(request,'article/post_list.html',{})
+
