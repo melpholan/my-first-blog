c92d72fd721ca36978fa520670364b16cc7cde5a
melpholan <melpholan@yandex.ru>
2016-11-20 17:35:40 +0300

    Добавлено DataSet в представление

--- article/views.py
+++ article/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from django.utils  import  timezone
+from article.models import Article
 
 # Create your views here.
 
 def post_list(request):
-    return render(request,'article/post_list.html',{})
+
+    article = Article.objects.filter(article_published_date__lte=timezone.now()).order_by('article_title')
+    return render(request,'article/post_list.html',{'article':article})
 
